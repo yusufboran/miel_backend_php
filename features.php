ba4872114cd6ad9dcfa8226b06029c83f4076a02
Yusuf Boran <yusuf.boran@rbbt.com.tr>
2023-03-29 15:31:39 +0300

    features fixed code

--- features.php
+++ features.php
@@ -4,40 +4,46 @@ require_once 'token.php';
 
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
     $sql = "SELECT * FROM features;";
-}
-
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-
+} elseif ($_SERVER['REQUEST_METHOD'] == "POST") {
     $method = $_POST['method'];
     $token = $_POST['token'];
 
     $isTokenValid = checkTokenValidity($dbconn, $token);
     if (!$isTokenValid) {
-        echo ("Token is invalid");
-        return;
+        die("Token is invalid");
     }
-    if ($method == "post" && !empty($_POST['title']) && !empty($_POST['trtext']) && !empty($_POST['entext'])) {
-        $sql = "INSERT INTO features (title, trtext,entext) VALUES 
-        ('" . $_POST['title'] . "','" . $_POST['trtext'] . "','" . $_POST['entext'] . "');";
 
-    } else if ($method == "delete" && !empty($_POST['id'])) {
-        $sql = "DELETE from features where id = '" . $_POST['id'] . "'";
-    } else if ($method == "update" && !empty($_POST['id']) && !empty($_POST['title']) && !empty($_POST['trtext']) && !empty($_POST['entext'])) {
-        $sql = "UPDATE features SET title='" . $_POST['title'] . "', trtext='" . $_POST['trtext'] . "',entext='" . $_POST['entext'] . "' WHERE id='" . $_POST['id'] . "'";
-    } else {
-        echo ("Token is invalid");
-        return;
+    switch ($method) {
+        case 'post':
+            if (!empty($_POST['title']) && !empty($_POST['trtext']) && !empty($_POST['entext'])) {
+                $sql = "INSERT INTO features (title, trtext,entext) VALUES ('{$_POST['title']}', '{$_POST['trtext']}', '{$_POST['entext']}')";
+            } else {
+                die("Missing fields");
+            }
+            break;
+        case 'delete':
+            if (!empty($_POST['id'])) {
+                $sql = "DELETE FROM features WHERE id = '{$_POST['id']}'";
+            } else {
+                die("Missing fields");
+            }
+            break;
+        case 'update':
+            if (!empty($_POST['id']) && !empty($_POST['title']) && !empty($_POST['trtext']) && !empty($_POST['entext'])) {
+                $sql = "UPDATE features SET title='{$_POST['title']}', trtext='{$_POST['trtext']}',entext='{$_POST['entext']}' WHERE id='{$_POST['id']}'";
+            } else {
+                die("Missing fields");
+            }
+            break;
+        default:
+            die("Invalid method");
     }
+} else {
+    die("Invalid request method");
 }
 
 $result = pg_query($dbconn, $sql) or die('Error message: ' . pg_last_error());
-if (!$result) {
-    echo pg_last_error($connection);
-} else {
-    $rows = pg_fetch_all($result);
-    echo json_encode($rows);
-    http_response_code(200);
-    exit;
-}
-return;
+$rows = pg_fetch_all($result);
+echo json_encode($rows);
+http_response_code(200);
 pg_close($dbconn);
