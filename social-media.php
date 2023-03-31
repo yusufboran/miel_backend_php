b0a80c98c271a6143878adf650d13a6047370976
Yusuf Boran <yusuf.boran@rbbt.com.tr>
2023-03-31 14:41:01 +0300

    check

--- social-media.php
+++ social-media.php
@@ -1,7 +1,13 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
+header("Content-type:application/json");
 require_once 'conn.php';
 require_once 'token.php';
- 
+
+$data = json_decode(file_get_contents("php://input"));
+
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
 
     $sql = "SELECT * FROM socialmedialist";
@@ -15,62 +21,32 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
         print "Error!: " . $e->getMessage() . "<br/>";
         die();
     }
-
 }
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
-    $method = $_POST['method'];
-    $token = $_POST['token'];
+    $socialmedia = $data->socialmedia;
+    $username = $data->username;
+    $sql = "INSERT INTO socialmedialist (socialmedia, username) VALUES ('" . $socialmedia . "', '" . $username . "');";
 
-    $isTokenValid = checkTokenValidity($dbconn, $token);
-    if (!$isTokenValid) {
-        echo ("Token is invalid");
-        return;
-    }
-
-    if ($method == "post") {
-
-        $socialmedia = $_POST['socialmedia'];
-        $username = $_POST['username'];
-        
-        if (!(isset($socialmedia) && isset($username) )) {
-            echo ("All input is required");
-            return;
-        }
-        $sql = "INSERT INTO socialmedialist (socialmedia, username) VALUES ('" . $socialmedia . "', '" . $username . "');";
-    }
-    if ($method == "delete") {
-
-        $id = $_POST['id'];
-
-        if (!(isset($id))) {
-            echo ("All input is required");
-            return;
-        }
-
-        $sql = "DELETE FROM socialmedialist where id = " . $id . ";";
-    }
-
-    if ($method == "update") {
-
-        $id = $_POST['id'];
-        $username = $_POST['username'];
-
-        if (!(isset($id) && isset($username)  )) {
-            echo ("All input is required");
-            return;
-        }
-        $sql = "UPDATE socialmedialist SET  username='".$username."' WHERE id=" . $id . ";";
-    }
-
-    $result = pg_query($dbconn, $sql) or die('Error message: ' . pg_last_error());
-    if (!$result) {
-        echo pg_last_error($connection);
-    } else {
-        http_response_code(200);
-        exit;
-    }
-    return;
 }
+if ($_SERVER['REQUEST_METHOD'] == "DELETE" && isset($data->id)) {
+    $sql = "DELETE FROM socialmedialist WHERE id = $data->id";
+} 
+if ($_SERVER['REQUEST_METHOD'] == "PUT") {
+
+    $id = $data->id;
+    $username = $data->username;
+    $sql = "UPDATE socialmedialist SET  username='" . $username . "' WHERE id=" . $id . ";";
+}
+
+
+$result = pg_query($dbconn, $sql) or die('Error message: ' . pg_last_error());
+if (!$result) {
+    echo pg_last_error($connection);
+} else {
+    http_response_code(200);
+    exit;
+}
+return;
 pg_close($dbconn);
